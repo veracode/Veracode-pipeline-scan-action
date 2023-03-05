960cf2cf8754669f80800d608ad630a4e3f33590
Julian Totzek-Hallhuber <j.totzek@gmail.com>
2023-03-05 11:53:36 +0100

    testing

--- src/index.ts
+++ src/index.ts
@@ -161,7 +161,7 @@ async function run (parameters:any){
     let pullRequest = process.env.GITHUB_REF
     let isPR:any = pullRequest?.indexOf("pull")
 
-    if ( isPR >= 1 && scanCommandOutput.trim() ){
+    if ( isPR >= 1 ){
         core.info("This run is part of a PR, should add some PR comment")
 
         const context = github.context
