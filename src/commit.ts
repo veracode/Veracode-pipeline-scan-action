ae2d392baede2da0e53f5b080b9dcf290430f5c4
Julian Totzek-Hallhuber <j.totzek@gmail.com>
2024-01-09 07:30:46 +0100

    update commit command

--- src/commit.ts
+++ src/commit.ts
@@ -48,9 +48,9 @@ export function commitBasline (parameters:any)  {
         let gitCommand = `git config --global user.name "${ process.env.GITHUB_ACTOR }"
         git config --global user.email "username@users.noreply.github.com"
         git add "${baselineFileName}"
-        git stash
+        git stash push -- ${baselineFileName}
         git pull origin ${parameters.store_baseline_file_branch} || echo "Couldn't find remote branch"
-        git checkout stash -- .
+        git checkout stash -- ${baselineFileName}
         git commit -m "Veracode Baseline File push from pipeline"
         git push origin HEAD:${parameters.store_baseline_file_branch} --force-with-lease`
 
